6c03eaa19207cc304bb16d997de71568cc15ae05
Ishvone <P2603585@my365.dmu.ac.uk>
2022-02-21 17:51:16 +0000

    Added testing for Orders class

---
+++ CarLtd Testing/tstOrders.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace CarLtd_Testing
+{
+    [TestClass]
+    public class tstOrders
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class we want to create
+            clsOrders Orders = new clsOrders();
+            //test to see that it exists
+            Assert.IsNotNull(Orders);
+        }
+    }
+
+    class clsOrders
+    {
+    }
+}
